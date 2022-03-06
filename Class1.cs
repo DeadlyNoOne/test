e695384f90676cc3dc4c70f3c3831f6cb6011d0d
Patryk Starus <s21962@pjwstk.edu.pl>
2022-03-06 12:34:19 +0100

    test

---
+++ Class1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Class1 : Interface1
+    {
+        private double _myProperty;
+        public float MyProperty { 
+            get { return (float)(_myProperty * 1.23); }
+            set { _myProperty = value; }
+        }
+
+        public int Zmienna { get; set; }
+
+        public virtual void fun()
+        {
+
+        }
+
+        public void Metoda()
+        {
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+                throw new Exception();
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace TestApp
+{
+    class Program
+    {
+        public async static Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            var c = new Class1();
+
+            // List<string> list = new List<string>();
+
+            var websiteUrl = args[0];
+            Console.WriteLine(websiteUrl);
+
+            var httpClient = new HttpClient();
+
+            var response = await httpClient.GetAsync(websiteUrl);
+            Console.WriteLine(response);
+            var content = await response.Content.ReadAsStringAsync();
+            Console.WriteLine(response);
+
+            var regex = new Regex(@"");
+
+            var matchCollection = regex.Matches(content);
+
+            foreach (var item in matchCollection)
+            {
+
+            }
+            
+
+        }
+    }
+}
